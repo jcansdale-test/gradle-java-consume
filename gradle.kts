df258a51bb0594e3317ab830c86b44f29efbd31c
jcansdale-test <60072076+jcansdale-test@users.noreply.github.com>
2020-06-25 17:03:51 +0100

    Update and rename plain.gradle.kts to gradle.kts

--- gradle.kts
+++ gradle.kts
@@ -22,13 +22,9 @@ version = "1.0-SNAPSHOT"
 
 dependencies {
     api("org.apache.commons:commons-math3:3.6.1")
-
     implementation("com.google.guava:guava:28.0-jre")
-
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.1")
-
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
-    
     my-dependency("org.test.plain:publish-java:1.0-SNAPSHOT")
 }
 
