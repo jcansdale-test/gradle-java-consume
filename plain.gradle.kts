18faa238595f6907f23548779e870c1d0a5e7dc0
jcansdale-test <60072076+jcansdale-test@users.noreply.github.com>
2020-06-25 17:03:26 +0100

    Update plain.gradle.kts

--- plain.gradle.kts
+++ plain.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
     
-    compile("org.test.plain:publish-java:1.0-SNAPSHOT")
+    my-dependency("org.test.plain:publish-java:1.0-SNAPSHOT")
 }
 
 val test by tasks.getting(Test::class) {
